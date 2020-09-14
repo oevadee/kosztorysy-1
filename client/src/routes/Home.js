9b659a496de97e1db43d7e18972b7f9376cc1325
Adrian <62460030+oevadee@users.noreply.github.com>
2020-09-14 21:43:34 +0200

    intro gotowe

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="introContainer">
       <h1 className="introContainer__header">Intro text</h1>
-      <p className="introContainer__description">Zatrudniamy i współpracujemy ze specjalistami z wieloletnim doświadczeniem.</p>
+      <p className="introContainer__description">Zatrudniamy i współpracujemy ze specjalistami <br /> z wieloletnim doświadczeniem.</p>
       <img className="introContainer__image" src="/img/intro.jpg" alt="" />
       <div className="introContainer__imageCover"></div>
       <button className="introContainer__button">Zobacz projekty</button>
