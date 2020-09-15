0a22f134da4d4d39956d3e7721e7e93e80b35179
Piotr Ostrowski <postrowski.00@gmail.com>
2020-09-15 15:56:27 +0200

    started contact page

--- client/src/routes/Contact.js
+++ client/src/routes/Contact.js
@@ -1,13 +1,48 @@
 import React from "react";
-
+import { Formik, Field } from "formik";
 
 // Components
 import Button from '../components/Button';
 
 const Contact = () => {
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (<>
     <h1>Kontakt</h1>
-    <Button />
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        message: ""
+      }}
+      onSubmit={handleSubmit}>
+        {(formProps) => (
+          <form onSubmit={formProps.handleSubmit} >
+            <label>Imię</label>
+            <Field name={"firstName"} type="text" />
+
+            <label>Nazwisko</label>
+            <Field name={"lastName"} type="text" />
+
+            <label>Adres email</label>
+            <Field name={"email"} type="text" />
+
+            <label>Numer telefonu</label>
+            <Field name={"email"} type="text" />
+
+            <label>Wiadomość</label>
+            <Field name={"message"} type="text" />
+            
+          </form>
+        )}
+
+
+    </Formik>
     </>
   );
 };
